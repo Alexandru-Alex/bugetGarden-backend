bd0fcf454eae34eacfbfe66bde531d2ec7e98920
Alexandru-Alex <alexstefan_ct@yahoo.com>
2026-04-04 01:25:59 +0300

    commit

---
+++ src/main/java/com/example/bugetGarden/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.bugetGarden.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoginController {
+
+    @PostMapping("login")
+    public void receiveEmail(@RequestBody GoogleToken token) {
+        log.info("TOKEN ESTE: {} ", token);
+    }
+
+}
